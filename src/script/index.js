39a435738ac82fe1b08b93fa6c2d3395fb7ebe72
Bil <gabriel1277lima@gmail.com>
2022-03-18 03:54:44 -0300

    refinando o código JS

--- src/script/index.js
+++ src/script/index.js
@@ -1,15 +1,18 @@
+/*TO DO
+    - reformular o visor, para que apresente o resultado em tempo real
+*/
 
-
-
+//função que pega o valor ao pressionar o botão
 function adicionarNumero(num){
-    // var pegando valor 
-    var numero = document.getElementById('visor').innerHTML;
-    document.getElementById('visor').innerHTML = '' + numero + num;
+    var numero = document.getElementById('visor').innerHTML; //acessa o visor
+    document.getElementById('visor').innerHTML = numero + num; //adiciona o número no visor
     
-
-
 }
+//função que realiza o cálculo matemático inserido
 function calculo(){
-    var resultado = document.getElementById('visor').innerHTML;
-    document.getElementById('visor').innerHTML = eval(resultado);
+
+    var resultado = document.getElementById('visor').innerHTML; // acessa o visor
+    if (resultado != '') {document.getElementById('visor').innerHTML = eval(resultado);} //caso encontre algum número no visor, faz o cálculo
+    
 }
+
